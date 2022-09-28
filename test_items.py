192ad2ef5f3bdca69e8ab133f21e5a6a2a5d5302
labeleeanny <https://github.com/labeleeanny>
2022-09-28 17:02:48 +0300

    update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_existence_of_the_button(browser):
     browser.get(link)
     time.sleep(30)
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed(), 'Кнопка добавления товара в корзину отсутствует'
